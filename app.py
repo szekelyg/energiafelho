ed222fb9ee4bcdcca504ff5639c864862ca33994
szekelyg <36407132+szekelyg@users.noreply.github.com>
2023-08-24 15:00:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    auth0_client_id = os.environ.get("AUTH0_CLIENT_ID", "Nem található a kliens ID")
+    return f'Hello World!<br>AUTH0_CLIENT_ID: {auth0_client_id}'
